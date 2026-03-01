10499e8772778a7b1f185dd6248d7d39123a60a9
BEN <benbasic@outlook.de>
2026-03-01 10:21:57 +0100

    worked on hero page

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeLogoBig(){
+    const contentLogoRef = document.getElementById('main_logo');
+    contentLogoRef.src = './assets/icons/Logo icon hover.svg';
+}
+
+function changeLogoSmall(){
+    const contentLogoRef = document.getElementById('main_logo');
+    contentLogoRef.src = './assets/icons/Logo icon.svg';
+}

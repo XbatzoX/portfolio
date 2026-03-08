b85843a086a94cca1668883904c25d2bf845d55d
BEN <benbasic@outlook.de>
2026-03-08 13:03:31 +0100

    added function for changing tab in project navigation bar

--- scripts/projects.js
+++ scripts/projects.js
@@ -4,9 +4,12 @@ let maxAmountOfProjects = 10;
 
 function activateNavigationTab(id){
     calculateAmountOfProjects();
+    removeBackgroundsFromTabs();
+    setBackgroundFromActiveTab(id);
 }
 
 function calculateAmountOfProjects(){
+    amountProjects = 0;
     let spanProject;
     for (let index = 0; index < maxAmountOfProjects; index++) {
         spanProject = document.getElementById(`project_${index}`);
@@ -22,6 +25,13 @@ function removeBackgroundsFromTabs(){
     let contentSpanRef;
     for (let index = 0; index < amountProjects; index++) {
         contentSpanRef = document.getElementById(`project_${index}`);
-        
+        contentSpanRef.style.color = '#89BCD9';
+        contentSpanRef.parentElement.classList.remove('tab-bg');
     }
-} 
+}
+
+function setBackgroundFromActiveTab(id){
+    let contentSpanRef = document.getElementById(id);
+    contentSpanRef.style.color = '#F8F5EC';
+    contentSpanRef.parentElement.classList.add('tab-bg');
+}

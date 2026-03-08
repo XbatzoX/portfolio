bc8152e7d79fb2ee3f4944920a09f1181c5d402e
BEN <benbasic@outlook.de>
2026-03-08 14:20:30 +0100

    added file for array of objects, filled with projects informations

---
+++ scripts/projectsObj.js
@@ -0,0 +1,18 @@
+const projects = [
+    {
+        "title" : "Join",
+        "duration" : "7 Wochen",
+        "list" : [{
+            "description" : "Kanban Board",
+            "content" : "Ein Projektmanagement-Tool im Kanban-Stil, inspiriert von Trello."
+        },
+        {
+            "description" : "Organisation meiner Aufgaben im Projekt",
+            "content" : "Um meinen Code sauber und wartbar zu halten, habe ich das Projekt in wiederverwendbare Module und Komponenten aufgeteilt. Dabei habe ich besonders auf eine klare Struktur, verständliche Benennung von Dateien, Variablen und Klassen sowie eine gute Dokumentation geachtet."
+        },
+        {
+            "description" : "Praxis der Teamarbeit",
+            "content" : "In unserem dreiköpfigen Projektteam war die Arbeit klar strukturiert. Mein Schwerpunkt lag auf dem Design sowie der Datenverwaltung für die Registrierung und die Verwaltung der Kontakte."
+        }]
+    }
+];

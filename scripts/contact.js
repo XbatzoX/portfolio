2339808f16ca40297e4d23e309a8057e5b102c67
BEN <benbasic@outlook.de>
2026-03-13 13:41:49 +0100

    worked on input fields of form

--- scripts/contact.js
+++ scripts/contact.js
@@ -28,3 +28,39 @@ function setContentNameOnFocus(){
     contentDivRef.classList.remove('input-height');
     contentSpanRef.innerHTML = 'Dein Name';
 }
+
+function checkMailInput(){
+    let contentInputRef = document.getElementById('input_mail');
+    let contentSpanRef = document.getElementById('input_mail_title');
+    let contentDivRef = document.getElementById('mail_input_box');
+    let validMail = checkValidEmail(String(contentInputRef.value));
+    if(validMail){
+        contentSpanRef.innerHTML = '';
+        contentInputRef.style.backgroundColor = 'transparent';
+        contentInputRef.classList.add('bg-img-done');
+        contentDivRef.classList.add('input-height');
+    }else{
+        contentDivRef.classList.remove('input-height');
+        contentSpanRef.innerHTML = 'Deine Email ist erforderlich';
+        contentSpanRef.style.color = "#E44C36";
+        contentInputRef.style.border = "1px solid #E44C36";
+        contentInputRef.style.backgroundColor = 'transparent';
+    }
+}
+
+function checkValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function setContentMailOnFocus(){
+    let contentInputRef = document.getElementById('input_mail');
+    let contentSpanRef = document.getElementById('input_mail_title');
+    let contentDivRef = document.getElementById('mail_input_box');
+    contentInputRef.classList.remove('bg-img-done');
+    contentInputRef.style.backgroundColor = "rgba(250, 250, 250, 0.1)";
+    contentInputRef.style.border = "1px solid #89BCD9";
+    contentSpanRef.style.color = "#89BCD9";
+    contentDivRef.classList.remove('input-height');
+    contentSpanRef.innerHTML = 'Deine Email';
+}

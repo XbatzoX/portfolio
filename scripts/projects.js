ba291ac3d4e8fd8e14a2a7cff59478d438cd57cc
BEN <benbasic@outlook.de>
2026-03-20 15:12:40 +0100

    finished my comments section in responsive

--- scripts/projects.js
+++ scripts/projects.js
@@ -88,29 +88,33 @@ function renderOngoingProject(projectId, divId){
 const slider = document.querySelector('.comment-container');
 
 let isDown = false;
+let isDragging = false;
 let startX;
 let scrollLeft;
 
 slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+    if (e.target.closest('.comment-text')) return;
+    isDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
 });
 
 slider.addEventListener('mouseleave', () => {
-  isDown = false;
+    isDown = false;
 });
 
 slider.addEventListener('mouseup', () => {
-  isDown = false;
+    isDown = false;
 });
 
 slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 1.5; 
-  slider.scrollLeft = scrollLeft - walk;
+    if (!isDown) return;
+
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.5; 
+    e.preventDefault();
+  
+    slider.scrollLeft = scrollLeft - walk;
 });
 
 function activateMobileNavigationTab(id, divId){

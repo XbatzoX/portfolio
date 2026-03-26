1e74aae7029d10d1749df36a7806c3f2d81aecff
BEN <benbasic@outlook.de>
2026-03-26 12:56:52 +0100

    added text content and footer in imprint.html

--- scripts/imprint.js
+++ scripts/imprint.js
@@ -5,3 +5,15 @@ function initImprint(){
 function goToHeroPage(){
     window.location.href = '../index.html#hero_section';
 }
+
+function goToContactSection(){
+    window.location.href = '../index.html#my_contact_section';
+}
+
+function githubImprintFooterAction(){
+    changeIcon('github_footer_mobile', '../assets/icons/mobile/footer_github_mobile_active.svg');
+    setTimeout(() => {
+        changeIcon('github_footer_mobile', '../assets/icons/mobile/footer_github_mobile.svg');
+    }, 800);
+    openWebsite('https://github.com/XbatzoX');
+}

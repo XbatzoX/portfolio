54f50e32a42fa8caa3d770cd1649eec51bd2fe7b
BEN <benbasic@outlook.de>
2026-03-26 16:43:32 +0100

    translation work on imprint

--- scripts/imprint.js
+++ scripts/imprint.js
@@ -20,6 +20,10 @@ function prepareLanguage(){
     return objData;
 }
 
+function scrollToTop(){
+    window.scrollTo(0,0);
+}
+
 function loadImprintLanguage(id, id_span){
     onFocusLanguageDesign(id, id_span);
     loadImprintLanguageData(languageID);
@@ -44,7 +48,12 @@ function githubImprintFooterAction(){
 function loadImprintLanguageData(languageId){
     loadNavData(languageId);
     loadCaptionData(languageId);
-
+    loadTermsData(languageId);
+    loadScopeData(languageId);
+    loadRightsData(languageId);
+    loadUseData(languageId);
+    loadDisclaimData(languageId);
+    loadIndemnityData(languageId);
 }
 
 function loadCaptionData(languageId){
@@ -56,6 +65,7 @@ function loadCaptionDataGerman(languageId){
     if(languageId == 0){
         document.getElementById('imprint_caption').innerHTML = imprintData.de.caption.imprintHead;
         document.getElementById('list_caption').innerHTML = imprintData.de.caption.imprintLine;
+        document.getElementById('contact_span').innerHTML = imprintData.de.contact;
     }
 }
 
@@ -63,5 +73,120 @@ function loadCaptionDataEnglish(languageId){
     if(languageId == 1){
         document.getElementById('imprint_caption').innerHTML = imprintData.en.caption.imprintHead;
         document.getElementById('list_caption').innerHTML = imprintData.en.caption.imprintLine;
+        document.getElementById('contact_span').innerHTML = imprintData.en.contact;
+    }
+}
+
+function loadTermsData(languageId){
+    loadTermsDataGerman(languageId);
+    loadTermsDataEnglish(languageId);
+}
+
+function loadTermsDataGerman(languageId){
+    if(languageId == 0){
+        document.getElementById('terms_span').innerHTML = imprintData.de.terms.head;
+        document.getElementById('terms_content').innerHTML = imprintData.de.terms.content;
+    }
+}
+
+function loadTermsDataEnglish(languageId){
+    if(languageId == 1){
+        document.getElementById('terms_span').innerHTML = imprintData.en.terms.head;
+        document.getElementById('terms_content').innerHTML = imprintData.en.terms.content;
+    }
+}
+
+function loadScopeData(languageId){
+    loadScopeDataGerman(languageId);
+    loadScopeDataEnglish(languageId);
+}
+
+function loadScopeDataGerman(languageId){
+    if(languageId == 0){
+        document.getElementById('scope_span').innerHTML = imprintData.de.scope.head;
+        document.getElementById('scope_content').innerHTML = imprintData.de.scope.content;
+    }
+}
+
+function loadScopeDataEnglish(languageId){
+    if(languageId == 1){
+        document.getElementById('scope_span').innerHTML = imprintData.en.scope.head;
+        document.getElementById('scope_content').innerHTML = imprintData.en.scope.content;
+    }
+}
+
+function loadRightsData(languageId){
+    loadRightsDataGerman(languageId);
+    loadRightsDataEnglish(languageId);
+}
+
+function loadRightsDataGerman(languageId){
+    if(languageId == 0){
+        document.getElementById('rights_span').innerHTML = imprintData.de.rights.head;
+        document.getElementById('rights_content').innerHTML = imprintData.de.rights.content;
+    }
+}
+
+function loadRightsDataEnglish(languageId){
+    if(languageId == 1){
+        document.getElementById('rights_span').innerHTML = imprintData.en.rights.head;
+        document.getElementById('rights_content').innerHTML = imprintData.en.rights.content;
+    }
+}
+
+function loadUseData(languageId){
+    loadUseDataGerman(languageId);
+    loadUseDataEnglish(languageId);
+}
+
+function loadUseDataGerman(languageId){
+    if(languageId == 0){
+        document.getElementById('use_span').innerHTML = imprintData.de.use.head;
+        document.getElementById('use_content').innerHTML = imprintData.de.use.content;
+    }
+}
+
+function loadUseDataEnglish(languageId){
+    if(languageId == 1){
+        document.getElementById('use_span').innerHTML = imprintData.en.use.head;
+        document.getElementById('use_content').innerHTML = imprintData.en.use.content;
+    }
+}
+
+function loadDisclaimData(languageId){
+    loadDisclaimDataGerman(languageId);
+    loadDisclaimDataEnglish(languageId);
+}
+
+function loadDisclaimDataGerman(languageId){
+    if(languageId == 0){
+        document.getElementById('disclaim_span').innerHTML = imprintData.de.disclaim.head;
+        document.getElementById('disclaim_content').innerHTML = imprintData.de.disclaim.content;
+    }
+}
+
+function loadDisclaimDataEnglish(languageId){
+    if(languageId == 1){
+        document.getElementById('disclaim_span').innerHTML = imprintData.en.disclaim.head;
+        document.getElementById('disclaim_content').innerHTML = imprintData.en.disclaim.content;
+    }
+}
+
+function loadIndemnityData(languageId){
+    loadIndemnityDataGerman(languageId);
+    loadIndemnityDataEnglish(languageId);
+}
+
+function loadIndemnityDataGerman(languageId){
+    if(languageId == 0){
+        document.getElementById('indemnity_span').innerHTML = imprintData.de.indemnity.head;
+        document.getElementById('indemnity_content').innerHTML = imprintData.de.indemnity.content;
+    }
+}
+
+function loadIndemnityDataEnglish(languageId){
+    if(languageId == 1){
+        document.getElementById('indemnity_span').innerHTML = imprintData.en.indemnity.head;
+        document.getElementById('indemnity_content').innerHTML = imprintData.en.indemnity.content;
     }
 }
--- scripts/imprintObj.js
+++ scripts/imprintObj.js
@@ -3,12 +3,114 @@ const imprintData = {
         "caption" : {
             "imprintHead" : "Impressum",
             "imprintLine" : "Angaben gemäß § 5 DDG"
+        },
+        "contact" : "Kontakt",
+        "terms" : {
+            "head" : "Akzeptanz der Nutzungsbedingungen",
+            "content" : `Durch den Zugriff auf und die Nutzung von <span class="highlight">Portfolio</span> (Produkt) erkennen Sie die folgenden Bedingungen sowie alle Richtlinien, 
+                        Hinweise oder Änderungen an, die Ihnen von Zeit zu Zeit vorgelegt werden können, und stimmen ihnen zu. Wir, die oben genannten Studierenden, 
+                        behalten uns vor, diese Bedingungen jederzeit und ohne vorherige Ankündigung zu ändern oder zu aktualisieren.`
+        },
+        "scope" : {
+            "head" : "Geltungsbereich und Eigentum am Produkt",
+            "content" : `<span class="highlight">Portfolio</span> wurde im Rahmen eines studentischen Gruppenprojekts in einem Webdevelopment-Bootcamp bei 
+                        der <a class="imprint-link" href="http://developerakademie.com" target="_blank">Developer Akademie GmbH</a> entwickelt. 
+                        Es dient ausschließlich Bildungszwecken und ist nicht für eine umfangreiche private oder geschäftliche Nutzung vorgesehen. 
+                        Daher können wir keine durchgehende Verfügbarkeit, Zuverlässigkeit, Genauigkeit oder sonstige Qualitätsmerkmale dieses Produkts garantieren. 
+                        Das Design von <span class="highlight">Portfolio</span> ist Eigentum der <a class="imprint-link" href="http://developerakademie.com" target="_blank">Developer Akademie GmbH</a>. 
+                        Jede unbefugte Nutzung, Reproduktion, Modifikation, 
+                        Verbreitung oder Vervielfältigung des Designs ist strengstens untersagt.`
+        },
+        "rights" : {
+            "head" : "Schutzrechte",
+            "content" : `Abgesehen vom Design, das der <a class="imprint-link" href="http://developerakademie.com" target="_blank">Developer Akademie GmbH</a> gehört, behalten wir, 
+                        die genannten Studierenden, sämtliche Schutzrechte an <span class="highlight">Portfolio</span>, 
+                        einschließlich aller damit verbundenen urheberrechtlich geschützten Materialien,
+                        Marken und sonstigen geschützten Informationen.`
+        },
+        "use" : {
+            "head" : "Nutzung des Produkts",
+            "content" : `<span class="highlight">Portfolio</span> darf ausschließlich für rechtmäßige Zwecke und im Einklang mit allen geltenden Gesetzen und Vorschriften genutzt werden. 
+                        Jede Nutzung von <span class="highlight">Portfolio</span> für illegale Aktivitäten oder zur Belästigung, Schädigung, 
+                        Bedrohung oder Einschüchterung anderer Personen ist streng verboten. 
+                        Sie sind allein verantwortlich für Ihre Interaktionen mit anderen Nutzern von <span class="highlight">Portfolio</span>.`
+        },
+        "disclaim" : {
+            "head" : "Haftungsausschluss und Haftungsbeschränkung",
+            "content" : `<span class="highlight">Portfolio</span> wird „wie gesehen“ und ohne jegliche ausdrückliche oder stillschweigende Gewährleistung bereitgestellt, 
+                        einschließlich, aber nicht beschränkt auf die stillschweigenden Gewährleistungen der Marktgängigkeit, 
+                        Eignung für einen bestimmten Zweck und Nichtverletzung von Rechten. In keinem Fall haften wir, die genannten Studierenden, 
+                        oder die <a class="imprint-link" href="http://developerakademie.com" target="_blank">Developer Akademie GmbH</a> für direkte, indirekte, zufällige, besondere, Folge- oder exemplarische Schäden, einschließlich, 
+                        aber nicht beschränkt auf Schäden aus entgangenem Gewinn, Goodwill, Nutzung, Daten oder anderen immateriellen Verlusten, 
+                        selbst wenn wir auf die Möglichkeit solcher Schäden hingewiesen wurden, 
+                        die sich aus der Nutzung oder Leistung von <span class="highlight">Portfolio</span> ergeben oder damit im Zusammenhang stehen.`
+        },
+        "indemnity" : {
+            "head" : "Freistellung",
+            "content" : `Sie erklären sich damit einverstanden, uns, die genannten Studierenden, die <a class="imprint-link" href="http://developerakademie.com" target="_blank">Developer Akademie GmbH</a> sowie unsere verbundenen Unternehmen, 
+                        Partner, leitenden Angestellten, Direktoren, 
+                        Vertreter und Mitarbeitenden von sämtlichen Ansprüchen, Forderungen, Verlusten, Schäden, Kosten oder 
+                        Haftungen (einschließlich angemessener Anwaltskosten) freizustellen, 
+                        die aus Ihrer Nutzung von <span class="highlight">Portfolio</span> und/oder Ihrem Verstoß gegen diesen Rechtshinweis entstehen.
+                        Datum: 26. März 2026`
         }
     },
     "en" : {
         "caption" : {
             "imprintHead" : "Legal notice",
             "imprintLine" : "Imprint"
+        },
+        "contact" : "Exploring the Board",
+        "terms" : {
+            "head" : "Acceptance of terms",
+            "content" : `By accessing and using <span class="highlight">Portfolio</span> (Product), you acknowledge and agree to the following terms and conditions, and any policies, 
+                        guidelines, or amendments thereto that may be presented to you from time to time. We, the listed students, 
+                        may update or change the terms and conditions from time to time without notice.`
+        },
+        "scope" : {
+            "head" : "Scope and ownership of the product",
+            "content" : `<span class="highlight">Portfolio</span> has been developed as part of a student group project in a web development bootcamp 
+                        at the <a class="imprint-link" href="http://developerakademie.com" target="_blank">Developer Akademie GmbH</a>. 
+                        It has an educational purpose and is not intended for extensive personal & business usage. 
+                        As such, we cannot guarantee consistent availability, reliability, accuracy, or any other aspect of quality regarding this Product. 
+                        The design of <span class="highlight">Portfolio</span> is owned by the <a class="imprint-link" href="http://developerakademie.com" target="_blank">Developer Akademie GmbH</a>. 
+                        Unauthorized use, reproduction, modification, distribution, 
+                        or replication of the design is strictly prohibited.`
+        },
+        "rights" : {
+            "head" : "Proprietary rights",
+            "content" : `Aside from the design owned by <a class="imprint-link" href="http://developerakademie.com" target="_blank">Developer Akademie GmbH</a>, we, 
+                        the listed students, retain all proprietary rights in <span class="highlight">Portfolio</span>, 
+                        including any associated copyrighted material, trademarks, and other proprietary information.`
+        },
+        "use" : {
+            "head" : "Use of the product",
+            "content" : `<span class="highlight">Portfolio</span> is intended to be used for lawful purposes only, in accordance with all applicable laws and regulations. 
+                        Any use of <span class="highlight">Portfolio</span> for illegal activities, or to harass, harm, threaten, 
+                        or intimidate another person, is strictly prohibited. 
+                        You are solely responsible for your interactions with other users of <span class="highlight">Portfolio</span>.`
+        },
+        "disclaim" : {
+            "head" : "Disclaimer of warranties and limitation of liability",
+            "content" : `<span class="highlight">Portfolio</span> is provided "as is" without warranty of any kind, whether express or implied, 
+                        including but not limited to the implied warranties of merchantability, 
+                        fitness for a particular purpose, and non-infringement. 
+                        In no event will we, the listed students, 
+                        or the <a class="imprint-link" href="http://developerakademie.com" target="_blank">Developer Akademie GmbH</a>, 
+                        be liable for any direct, indirect, incidental, special, 
+                        consequential or exemplary damages, including but not limited to, 
+                        damages for loss of profits, goodwill, use, data, or other intangible losses, 
+                        even if we have been advised of the possibility of such damages, 
+                        arising out of or in connection with the use or performance of <span class="highlight">Portfolio</span>.`
+        },
+        "indemnity" : {
+            "head" : "Indemnity",
+            "content" : `You agree to indemnify, defend and hold harmless us, the listed students, 
+                        the <a class="imprint-link" href="http://developerakademie.com" target="_blank">Developer Akademie GmbH</a>, 
+                        and our affiliates, partners, officers, directors, agents, and employees, from and against any claim, demand, 
+                        loss, damage, cost, or liability (including reasonable legal fees) arising out of or relating to your 
+                        use of <span class="highlight">Portfolio</span> and/or your breach of this Legal Notice.
+                        <br> March 26, 2026`
         }
     }
 }

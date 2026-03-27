b850aa091031fb435cb41533666828759eb2ed22
BEN <benbasic@outlook.de>
2026-03-27 10:56:38 +0100

    started with translation on policy sheet

--- scripts/policy.js
+++ scripts/policy.js
@@ -2,5 +2,36 @@ function initPolicy(){
     checkLocalStorage();
     let objData = prepareLanguage();
     onFocusLanguageDesign(objData.language, objData.languageSpan);
-    // loadImprintLanguageData(languageID);
+    loadPolicyLanguageData(languageID);
+}
+
+function loadPolicyLanguage(id, id_span){
+    onFocusLanguageDesign(id, id_span);
+    loadPolicyLanguageData(languageID);
+}
+
+function loadPolicyLanguageData(languageId){
+    loadNavData(languageId);
+    loadCaptionAndGlanceData(languageId);
+}
+
+function loadCaptionAndGlanceData(languageId){
+    loadCaptionAndGlanceDataGerman(languageId);
+    loadCaptionAndGlanceDataEnglish(languageId);
+}
+
+function loadCaptionAndGlanceDataGerman(languageId){
+    if(languageId == 0){
+        document.getElementById('policy_caption').innerHTML = policyData.de.captionAndGlance.caption;
+        document.getElementById('glance_span').innerHTML = policyData.de.captionAndGlance.head;
+        document.getElementById('glance_content').innerHTML = policyData.de.captionAndGlance.content;
+    }
+}
+
+function loadCaptionAndGlanceDataEnglish(languageId){
+    if(languageId == 1){
+        document.getElementById('policy_caption').innerHTML = policyData.en.captionAndGlance.caption;
+        document.getElementById('glance_span').innerHTML = policyData.en.captionAndGlance.head;
+        document.getElementById('glance_content').innerHTML = policyData.en.captionAndGlance.content;
+    }
 }
---
+++ scripts/policyObj.js
@@ -0,0 +1,19 @@
+const policyData = {
+    "de" : {
+        "captionAndGlance" : {
+            "caption" : "Datenschutzrichtlinie",
+            "head" : "1. Datenschutz auf einen Blick",
+            "content" : `Die folgenden Hinweise geben einen einfachen Überblick darüber, was mit Ihren personenbezogenen Daten passiert, 
+                        wenn Sie diese Website besuchen. 
+                        Personenbezogene Daten sind alle Daten, mit denen Sie persönlich identifiziert werden können.`
+        }
+    },
+    "en" : {
+        "captionAndGlance" : {
+            "caption" : "Privacy policy",
+            "head" : "1. Data Protection at a Glance",
+            "content" : `The following information provides a simple overview of what happens to your personal data when you 
+                        visit this website. Personal data is any data that can be used to personally identify you.`
+        }
+    }
+}

681146b8328ac1775bbfcfe740bcbe1ad05698d8
BEN <benbasic@outlook.de>
2026-03-28 15:39:10 +0100

    changed address

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@ header("Content-Type: application/json; charset=utf-8");
 // ------------------------------------------------------------
 
 // >>> DEINE EMAIL HIER EINTRAGEN <<<
-$siteEmail = "benmahal@icloud.com";
+$siteEmail = "benbasic@outlook.de";
 
 switch ($_SERVER['REQUEST_METHOD']) {
 

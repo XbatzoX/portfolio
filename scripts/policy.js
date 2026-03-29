9fa8d06c49ce8394bf858010a68a2c8a8126a68f
BEN <benbasic@outlook.de>
2026-03-29 14:05:00 +0200

    JSDoc

--- scripts/policy.js
+++ scripts/policy.js
@@ -1,3 +1,4 @@
+/** This function is used to load the data in correct language on private policy site */
 function initPolicy(){
     checkLocalStorage();
     let objData = prepareLanguage();
@@ -5,11 +6,20 @@ function initPolicy(){
     loadPolicyLanguageData(languageID);
 }
 
+/**
+ * This function load the correct languge data an designs the language buttons
+ * @param {string} id - includes the id of language button container 
+ * @param {string} id_span - includes the id of language button text 
+ */
 function loadPolicyLanguage(id, id_span){
     onFocusLanguageDesign(id, id_span);
     loadPolicyLanguageData(languageID);
 }
 
+/**
+ * This function loads the data of policy page in the correct language
+ * @param {number} languageId - includes the number of language ID 
+ */
 function loadPolicyLanguageData(languageId){
     loadNavData(languageId);
     loadCaptionAndGlanceData(languageId);
@@ -18,11 +28,19 @@ function loadPolicyLanguageData(languageId){
     loadPolicyRightsData(languageId);
 }
 
+/**
+ * This function loads caption and glance data in correct language
+ * @param {number} languageId - includes the number of language ID  
+ */
 function loadCaptionAndGlanceData(languageId){
     loadCaptionAndGlanceDataGerman(languageId);
     loadCaptionAndGlanceDataEnglish(languageId);
 }
 
+/**
+ * This function loads caption and glance data in german
+ * @param {number} languageId - includes the number of language ID  
+ */
 function loadCaptionAndGlanceDataGerman(languageId){
     if(languageId == 0){
         document.getElementById('policy_caption').innerHTML = policyData.de.captionAndGlance.caption;
@@ -31,6 +49,10 @@ function loadCaptionAndGlanceDataGerman(languageId){
     }
 }
 
+/**
+ * This function loads caption and glane data in english
+ * @param {number} languageId - includes the number of language ID  
+ */
 function loadCaptionAndGlanceDataEnglish(languageId){
     if(languageId == 1){
         document.getElementById('policy_caption').innerHTML = policyData.en.captionAndGlance.caption;
@@ -39,11 +61,19 @@ function loadCaptionAndGlanceDataEnglish(languageId){
     }
 }
 
+/**
+ * This function loads collection data in the correct language
+ * @param {number} languageId - includes the number of language ID  
+ */
 function loadCollectionData(languageId){
     loadCollectionDataGerman(languageId);
     loadCollectionDataEnglish(languageId);
 }
 
+/**
+ * This function loads collection data in german
+ * @param {number} languageId - includes the number of language ID  
+ */
 function loadCollectionDataGerman(languageId){
     if(languageId == 0){
         document.getElementById('collection_span').innerHTML = policyData.de.collection.head;
@@ -51,6 +81,10 @@ function loadCollectionDataGerman(languageId){
     }
 }
 
+/**
+ * This function loads collection data in english
+ * @param {number} languageId - includes the number of language ID  
+ */
 function loadCollectionDataEnglish(languageId){
     if(languageId == 1){
         document.getElementById('collection_span').innerHTML = policyData.en.collection.head;
@@ -58,11 +92,19 @@ function loadCollectionDataEnglish(languageId){
     }
 }
 
+/**
+ * This function loads hosting data in correct langauage
+ * @param {number} languageId - includes the number of language ID  
+ */
 function loadHostingData(languageId){
     loadHostingDataGerman(languageId);
     loadHostingDataEnglish(languageId);
 }
 
+/**
+ * This function loads hosting data in german
+ * @param {number} languageId - includes the number of language ID  
+ */
 function loadHostingDataGerman(languageId){
     if(languageId == 0){
         document.getElementById('hosting_span').innerHTML = policyData.de.hosting.head;
@@ -74,6 +116,10 @@ function loadHostingDataGerman(languageId){
     }
 }
 
+/**
+ * This function loads hosting data in english 
+ * @param {number} languageId - includes the number of language ID  
+ */
 function loadHostingDataEnglish(languageId){
     if(languageId == 1){
         document.getElementById('hosting_span').innerHTML = policyData.en.hosting.head;
@@ -85,11 +131,19 @@ function loadHostingDataEnglish(languageId){
     }
 }
 
+/**
+ * This function loads policy rights data in correct language
+ * @param {number} languageId - includes the number of language ID  
+ */
 function loadPolicyRightsData(languageId){
     loadPolicyRightsDataGerman(languageId);
     loadPolicyRightsDataEnglish(languageId);
 }
 
+/**
+ * This function loads policy rights data in german
+ * @param {number} languageId - includes the number of language ID  
+ */
 function loadPolicyRightsDataGerman(languageId){
     if(languageId ==  0){
         document.getElementById('policyRights_span').innerHTML = policyData.de.policyRights.head;
@@ -97,6 +151,10 @@ function loadPolicyRightsDataGerman(languageId){
     }
 }
 
+/**
+ * This function loads policy rights data in english
+ * @param {number} languageId - includes the number of language ID  
+ */
 function loadPolicyRightsDataEnglish(languageId){
     if(languageId ==  1){
         document.getElementById('policyRights_span').innerHTML = policyData.en.policyRights.head;

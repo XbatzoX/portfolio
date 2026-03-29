35def417fe67b4cbe364edc8a201a3c542eabdd0
BEN <benbasic@outlook.de>
2026-03-29 14:25:28 +0200

    JSDoc

--- scripts/projects.js
+++ scripts/projects.js
@@ -3,6 +3,11 @@ let amountProjects = 0;
 let maxAmountOfProjects = 10;
 let projectLanguageId = 0;
 
+/**
+ * This function is used to activate the navigation bar on my projects section
+ * @param {string} id - includes the id of project text
+ * @param {string} divId - includes the id of project tab container
+ */
 function activateNavigationTab(id, divId){
     calculateAmountOfProjects();
     removeBackgroundsFromTabs();
@@ -11,11 +16,15 @@ function activateNavigationTab(id, divId){
     checkIfOngoingProject(projectId, divId);
 }
 
+/**
+ * This function is used to set the correct language on my projects section
+ */
 function setProjectLanguage(){
     checkIfOngoingProject(projectId, 'project_content');
     checkIfOngoingProjectMobile(projectId, 'mobile_project_content');
 }
 
+/** This function is used to calculate the amount of projects in projectObj */
 function calculateAmountOfProjects(){
     amountProjects = 0;
     let spanProject;
@@ -31,6 +40,7 @@ function calculateAmountOfProjects(){
     }
 }
 
+/*** This function remove background designs from navigation tab*/
 function removeBackgroundsFromTabs(){
     let contentSpanRef;
     for (let index = 0; index < amountProjects; index++) {
@@ -40,12 +50,17 @@ function removeBackgroundsFromTabs(){
     }
 }
 
+/**
+ * This function is used to set the design from active tab
+ * @param {string} id - includes id from project tab text 
+ */
 function setBackgroundFromActiveTab(id){
     let contentSpanRef = document.getElementById(id);
     contentSpanRef.style.color = '#F8F5EC';
     contentSpanRef.parentElement.classList.add('tab-bg');
 }
 
+/** This function sets the projectId */
 function setProjectId(id){
     for (let index = 0; index < amountProjects; index++) {
         if((id == 'project_' + index) || (id == 'mobile_project_' + index)){
@@ -57,6 +72,7 @@ function setProjectId(id){
     }
 }
 
+/** This function is used to check if ongoing project is clicked */
 function checkIfOngoingProject(projectId, divId){
     if(projectId == (amountProjects - 1)){
         renderOngoingProject(projectId, divId);
@@ -65,6 +81,11 @@ function checkIfOngoingProject(projectId, divId){
     }
 }
 
+/**
+ * This function renders the project content in correct language
+ * @param {number} projectId - includes the projectId from active project 
+ * @param {string} divId - includes the id from container where content will be rendered 
+ */
 function renderProjectInfos(projectId, divId){
     let contentRef = document.getElementById(divId);
     contentRef.innerHTML = '';
@@ -75,6 +96,11 @@ function renderProjectInfos(projectId, divId){
     }
 }
 
+/**
+ * This function renders the content of ongoing project in correct language
+ * @param {string} projectId - includes the projectId from active project  
+ * @param {string} divId - includes the id from container where content will be rendered 
+ */
 function renderOngoingProject(projectId, divId){
     let contentRef = document.getElementById(divId);
     contentRef.innerHTML = '';
@@ -92,6 +118,7 @@ let isDragging = false;
 let startX;
 let scrollLeft;
 
+/** event listener for slider function at comments container */
 slider.addEventListener('mousedown', (e) => {
     if (e.target.closest('.comment-text')) return;
     isDown = true;
@@ -99,14 +126,17 @@ slider.addEventListener('mousedown', (e) => {
     scrollLeft = slider.scrollLeft;
 });
 
+/** event listener for slider function at comments container */
 slider.addEventListener('mouseleave', () => {
     isDown = false;
 });
 
+/** event listener for slider function at comments container */
 slider.addEventListener('mouseup', () => {
     isDown = false;
 });
 
+/** event listener for slider function at comments container */
 slider.addEventListener('mousemove', (e) => {
     if (!isDown) return;
 
@@ -117,6 +147,11 @@ slider.addEventListener('mousemove', (e) => {
     slider.scrollLeft = scrollLeft - walk;
 });
 
+/**
+ * This function is used to activate the navigation tab in mobile view
+ * @param {string} id - includes the id of project text 
+ * @param {string} divId - includes the id of navigation tab container 
+ */
 function activateMobileNavigationTab(id, divId){
     calculateAmountOfProjects();
     removeBackgroundsFromMobileTabs();
@@ -125,6 +160,9 @@ function activateMobileNavigationTab(id, divId){
     checkIfOngoingProjectMobile(projectId, divId);
 }
 
+/**
+ * This function removes the backround design from mobile navigation tabs
+ */
 function removeBackgroundsFromMobileTabs(){
     let contentSpanRef;
     for (let index = 0; index < amountProjects; index++) {
@@ -134,6 +172,10 @@ function removeBackgroundsFromMobileTabs(){
     }
 }
 
+/**
+ * This function set the design of active project tab
+ * @param {string} id - includes the id of active navigation tab on my projects section 
+ */
 function setBackgroundFromActiveMobileTab(id){
     let contentSpanRef = document.getElementById(id);
     contentSpanRef.style.color = '#89BCD9';
@@ -141,6 +183,10 @@ function setBackgroundFromActiveMobileTab(id){
     setBorderRadiusOfBox(id);
 }
 
+/**
+ * This function sets the border in differnt situations of mobile project navigation section
+ * @param {string} id - includes the id of active navigation tab on my projects section 
+ */
 function setBorderRadiusOfBox(id){
     document.getElementById('mobile_content_frame').style.borderTopRightRadius = "30px";
     document.getElementById('mobile_content_frame').style.borderTopLeftRadius = "30px";
@@ -152,6 +198,11 @@ function setBorderRadiusOfBox(id){
     }
 }
 
+/**
+ * 
+ * @param {number} projectId - includes the projectId of active project 
+ * @param {string} divId - includes the id of div container where the data will be rendered 
+ */
 function checkIfOngoingProjectMobile(projectId, divId){
     if(projectId == (amountProjects - 1)){
         renderOngoingProjectMobile(projectId, divId);
@@ -160,6 +211,11 @@ function checkIfOngoingProjectMobile(projectId, divId){
     }
 }
 
+/**
+ * This function renders the project content at mobile view in the correct language
+ * @param {number} projectId  - includes the projectId of active project 
+ * @param {string} divId - includes the id of div container where the data will be rendered 
+ */
 function renderProjectInfosMobile(projectId, divId){
     let contentRef = document.getElementById(divId);
     contentRef.innerHTML = '';
@@ -170,6 +226,11 @@ function renderProjectInfosMobile(projectId, divId){
     }
 }
 
+/**
+ * This function renders the content of ongoing project in correct language (mobile view)
+ * @param {number} projectId - includes the projectId of active project 
+ * @param {string} divId - includes the id of div container where the data will be rendered 
+ */
 function renderOngoingProjectMobile(projectId, divId){
     let contentRef = document.getElementById(divId);
     contentRef.innerHTML = '';
